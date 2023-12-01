013d4474bac19e241aafc3f02b01794257371028
sychotix <8293864+sychotixdev@users.noreply.github.com>
2023-12-01 14:42:55 +0100

    Initial commit

---
+++ InputHumanizerDelegates.cs
@@ -0,0 +1,11 @@
+﻿using ExileCore.Shared;
+using InputHumanizer.Input;
+
+namespace InputHumanizerLib
+{
+    public class InputHumanizerDelegates
+    {
+        public delegate bool TryGetInputControllerDelegate(string requestingPlugin, out IInputController controller);
+        public delegate SyncTask<IInputController> GetInputControllerDelegate(string requestingPlugin, TimeSpan waitTime);
+    }
+}
