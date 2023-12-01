5cc82c777dad7efd4401b23ba4e3f34efb01f71d
sychotix <8293864+sychotixdev@users.noreply.github.com>
2023-12-01 18:33:28 +0100

    Add MouseButtons to mouse methods and a new VerticalScroll method

--- IInputController.cs
+++ IInputController.cs
@@ -11,7 +11,10 @@ namespace InputHumanizer.Input
         bool KeyDown(Keys key);
         SyncTask<bool> KeyUp(Keys key, bool releaseImmediately = false, CancellationToken cancellationToken = default);
         SyncTask<bool> Click(CancellationToken cancellationToken = default);
-        SyncTask<bool> Click(Vector2 coordinate, CancellationToken cancellationToken = default);
+        SyncTask<bool> Click(MouseButtons button, CancellationToken cancellationToken = default);
+        SyncTask<bool> Click(MouseButtons button, Vector2 coordinate, CancellationToken cancellationToken = default);
+        SyncTask<bool> VerticalScroll(bool forward, int numClicks, CancellationToken cancellationToken = default);
+        SyncTask<bool> VerticalScroll(bool forward, int numClicks, Vector2? coordinate, CancellationToken cancellationToken = default);
         SyncTask<bool> MoveMouse(Vector2 coordinate, CancellationToken cancellationToken = default);
         SyncTask<bool> MoveMouse(Vector2 coordinate, int maxInterpolationDistance, int minInterpolationDelay, int maxInterpolationDelay, CancellationToken cancellationToken = default);
         int GenerateDelay();
